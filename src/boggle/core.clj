ce3cb4ffed514b4a4188c3186f683dd925e37eb8
Derek Mansen <dmansen@rjmetrics.com>
2011-12-14 23:15:03 -0500

    changed data structures a bunch

--- src/boggle/core.clj
+++ src/boggle/core.clj
@@ -41,11 +41,6 @@
    \E \I \M \O
    \T \O \A \E])
 
-(defn safe-access
-  [vec]
-  "Wraps safe access around this vector (catches IndexOutOfBoundsException and returns nil instead. This is stupid."
-  )
-
 (defn board-length
   "one side of the board"
   [board]
@@ -53,28 +48,33 @@
 
 (defn letter-at
   [board [x y]]
-  (try (board (+ x (* y (board-length board))))
-    (catch IndexOutOfBoundsException e nil)))
+  (board (+ x (* y (board-length board)))))
 
 (defn neighbors
   "Given a board and a position, builds the neighbor set for the letter at that position on the board."
   [board [x y]]
-  (filter (fn [x] (not (nil? x)))
-          [(letter-at board [y (dec x)])
-           (letter-at board [(dec y) (dec x)])
-           (letter-at board [(dec y) x])
-           (letter-at board [(dec y) (inc x)])
-           (letter-at board [y (inc x)])
-           (letter-at board [(inc y) (inc x)])
-           (letter-at board [(inc y) x])
-           (letter-at board [(inc y) (dec x)])]))
+  (map (fn [pos]
+         {(letter-at board pos) pos})
+       (filter (fn [[x y]]
+                 (and (> x -1)
+                      (> y -1)
+                      (< x (board-length board))
+                      (< y (board-length board))))
+               [[y (dec x)]
+                [(dec y) (dec x)]
+                [(dec y) x]
+                [(dec y) (inc x)]
+                [y (inc x)]
+                [(inc y) (inc x)]
+                [(inc y) x]
+                [(inc y) (dec x)]])))
 
 (defn neighbor-map
   "builds the neighbor map for this board"
   [board]
-  (map (fn [pos]
-         [(letter-at board pos) (neighbors board pos)])
-       (all-positions board)))
+  (vec (map (fn [pos]
+              [(letter-at board pos) (neighbors board pos)])
+            (all-positions board))))
 
 (defn all-positions
   "returns a vector of all positions on the board"
@@ -83,8 +83,9 @@
         y (range 0 (board-length board))]
     [x y]))
 
-(defn word-accessible?
-  [neighbor-map word]
-  (let [first-char (.charAt word 0)]
-    (if (get neighbor-map first-char)
-      )))
+(defn trace-word-inner
+  [board trace word]
+  (let [current-pos (last trace)
+        first-char (.charAt word 0)]
+    (let [cur-neighbors (neighbors board current-pos)]
+      "not implemented")))
