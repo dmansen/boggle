496723f84e78a92b0ce2efcaf1285ba1b7451d3b
Derek Mansen <dmansen@rjmetrics.com>
2011-12-15 07:55:21 -0500

    use better selectors and constructors

--- src/boggle/dictionary.clj
+++ src/boggle/dictionary.clj
@@ -7,14 +7,23 @@
        (> (.length s) 2)
        (< (.length s) 6)))
 
+(defn words-starting-with
+  "List of words that start with the specified letter, from the dictionary provided."
+  [dict char]
+  (get dict char))
+
+(defn empty-word-set
+  []
+  #{})
+
 (defn add-word
   "adds a word to the dictionary"
   [dict word]
   (let [first-char (.charAt word 0)]
     (let [current-word-set
-          (if (nil? (get dict first-char))
-            #{}
-            (get dict first-char))]
+          (if (nil? (words-starting-with dict first-char))
+            (empty-word-set)
+            (words-starting-with dict first-char))]
       (assoc dict first-char (conj current-word-set word)))))
 
 (defn load-dictionary
@@ -29,8 +38,3 @@
           (if (good-word? new-word)
             (recur (add-word dict new-word))
             (recur dict)))))))
-
-(defn words-starting-with
-  "List of words that start with the specified letter, from the dictionary provided."
-  [dict char]
-  (get dict char))
