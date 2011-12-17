d31662fee0ad25ce8f228f372ab3f2582f9da03e
Derek Mansen <dmansen@rjmetrics.com>
2011-12-16 23:20:35 -0500

    Lots of unnecessary functions deleted

--- src/boggle/board.clj
+++ src/boggle/board.clj
@@ -1,22 +1,20 @@
 (ns boggle.board)
 
-(defn letters
+(defn- letters
   "gets the letters on the board"
   [board]
   board)
 
-(defn board-length
+(defn- board-length
   "one side of the board"
   [board]
   (int (Math/sqrt (count board))))
 
-(defn letter-at
+(defn- letter-at
   [board [x y]]
   (board (+ x (* y (board-length board)))))
 
-(def letter-and-neighbors-at letter-at)
-
-(defn neighbor-positions
+(defn- neighbor-positions
   "Ugly function, so it's here to not look at again. Given a board and a position, returns a vector of all valid neighbor positions."
   [board [x y]]
   (filter (fn [[x y]]
@@ -33,36 +31,24 @@
            [x (inc y)]
            [(dec x) (inc y)]]))
 
-(defn neighbors
-  "Given a board and a position, builds the neighbor set for the letter at that position on the board."
-  [board pos]
-  (vec (map (fn [p]
-              [(letter-at board p) p])
-            (neighbor-positions board pos))))
-
-(defn all-positions
+(defn- all-positions
   "returns a vector of all positions on the board"
   [board]
   (vec (for [y (range 0 (board-length board))
              x (range 0 (board-length board))]
          [x y])))
 
-(defn neighbor-map
-  "builds the neighbor map for this board"
-  [board]
-  (vec (map (fn [pos]
-              [(letter-at board pos) (neighbors board pos)])
-            (all-positions board))))
-
-(defn neighbors-at
+(defn- neighbors
   [board pos]
-  ((letter-and-neighbors-at (neighbor-map board) pos) 1))
+  (map
+   (fn [pos] [(letter-at board pos) pos])
+   (neighbor-positions board pos)))
 
-(defn next-letter-options
+(defn- next-letter-options
   "returns a vector of possible positions to continue the trace"
   [board trace current-pos word]
-  (vec (map #(% 1)
-            (let [ns (neighbors-at sample-board current-pos)]
+  (vec (map second
+            (let [ns (neighbors sample-board current-pos)]
               (filter
                (fn [[letter pos]]
                  (and
@@ -70,30 +56,31 @@
                   (not (some #{pos} trace))))
                ns)))))
 
-(defn letters-to-positions
+(defn- letters-to-positions
   [board]
-  (map (fn [x y] [x y])
+  (map (fn [c pos] [c pos])
        board
        (all-positions board)))
 
-(defn comp-n
+(defn- comp-n
   "composes f with the argument n times"
   [f n x]
   (if (= 0 n)
     x
     (recur f (dec n) (f x))))
 
-(defn trace-word-inner
+(defn- trace-word-inner
   [board trace current-pos word]
   (if (empty? word)
     (conj trace current-pos)
-    (for [next-pos (next-letter-options board trace current-pos word)]
+    (for [next-pos
+          (next-letter-options board trace current-pos word)]
       (trace-word-inner board
                         (conj trace current-pos)
                         next-pos
                         (.substring word 1)))))
 
-(defn trace-word-unwrapper
+(defn- trace-word-unwrapper
   [board trace current-pos word]
   "need to do this because the inner wrapper keeps wrapping the results in more and more lists...this is probably a dumb way of doing this"
   (vec (comp-n first (.length word)
@@ -103,6 +90,9 @@
   [board word]
   (vec (for [[c pos] (letters-to-positions board)
              :when (= c (.charAt word 0))
-             :let [trace (trace-word-unwrapper board [] pos (.substring word 1))]
+             :let [trace (trace-word-unwrapper board
+                                               []
+                                               pos
+                                               (.substring word 1))]
              :when (not (empty? trace))]
          trace)))
